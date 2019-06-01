200654db921d71d57785893458309fb1135017be
braham delam <brahamdelam@brahams-MacBook-Air.local>
2019-05-31 20:17:44 -0400

    yo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navmenu from "./NavMenu";
+import logo from "./logo.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    let links = [
+      { label: "Your Home", link: "#home", active: true },
+      { label: "Stats", link: "#about" },
+      { label: "Portfolio", link: "#portfolio" },
+      { label: "Contact Us", link: "#contact-us" }
+    ];
+
+    return (
+      <div>
+        <Navmenu links={links} logo={logo} />
+      </div>
+    );
+  }
 }
 
 export default App;
